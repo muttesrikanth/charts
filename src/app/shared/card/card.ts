d77575fdb84819799df5f180febbbd22615135e9
muttesrikanth <srikanth12cn27@gmail.com>
2025-10-27 21:35:57 +0530

    card implemented

--- src/app/shared/card/card.ts
+++ src/app/shared/card/card.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Signal, signal } from '@angular/core';
+import { CardInterface } from '../../core/interfaces/interfaces.card';
+import { CardService } from '../../core/services/card-service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card.html',
   styleUrl: './card.scss',
 })
 export class Card {
+ private employeeService = inject(CardService);
 
+  employee:Signal<CardInterface> = this.employeeService.getEmployee()
 }
